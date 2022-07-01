11b31f7f9544b7b278fb247244950889d7ac3c0c
mo <huxiajie@outlook.com>
2022-07-01 11:19:01 +0800

    organize python_in_action

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
-def select_sort():
-    pass
+def select_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
 
 def bubble_sort():
     pass
 
+
 if __name__ == "__main__":
-    pass
+    for i in range(2):
+        print(i)
