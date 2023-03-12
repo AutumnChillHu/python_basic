1685a34eddc3ce5f81d4562c45b03eceaab8dcfe
mo <huxiajie@outlook.com>
2023-03-12 15:12:16 +0800

    0312 before next start

--- concurrency/threadingMO.py
+++ concurrency/threadingMO.py
@@ -16,72 +16,65 @@
 import threading
 import time
 
-from decorators.timer import timer
 
-
-def run_long(msg):
+def run_task1():
     print(
-        "线程开始run>>> fun_name={} thread_name={} args={}".format(run_long.__name__, threading.current_thread().name, msg))
-    time.sleep(4)
-    print(
-        "线程结束run>>> fun_name={} thread_name={} args={}".format(run_long.__name__, threading.current_thread().name, msg))
-
-
-def run_short(msg):
-    print("线程开始run>>> fun_name={} thread_name={} args={}".format(run_short.__name__, threading.current_thread().name,
-                                                                 msg))
+        "线程开始run>>> fun_name={} thread_name={}".format(run_task1.__name__, threading.current_thread().name))
     time.sleep(2)
-    print("线程结束run>>> fun_name={} thread_name={} args={}".format(run_short.__name__, threading.current_thread().name,
-                                                                 msg))
+    print(
+        "线程结束run>>> fun_name={} thread_name={}".format(run_task1.__name__, threading.current_thread().name))
+
+
+def run_task2():
+    print("线程开始run>>> fun_name={} thread_name={}".format(run_task2.__name__, threading.current_thread().name))
+    time.sleep(4)
+    print("线程结束run>>> fun_name={} thread_name={}".format(run_task2.__name__, threading.current_thread().name))
 
 
 def run(seconds):
-    print("线程开始run>>> fun_name={} thread_name={} args={}".format(__name__, threading.current_thread().name, seconds))
+    print(
+        "线程开始run>>> fun_name={} thread_name={} args={}".format(run.__name__, threading.current_thread().name, seconds))
     time.sleep(seconds)
-    print("线程结束run>>> fun_name={} thread_name={} args={}".format(__name__, threading.current_thread().name, seconds))
+    print(
+        "线程结束run>>> fun_name={} thread_name={} args={}".format(run.__name__, threading.current_thread().name, seconds))
 
 
 """实现多线程-方式1：threading.Thread类"""
 from threading import Thread
 
 
-@timer
 def new_threading():
+    print("方法开始>>> fun_name={} thread_name={}".format(new_threading.__name__, threading.current_thread().name))
+
+    # 下方介绍例子1：多线程执行多个无序性的、独立的、不同的子任务
     threading_list = []
     # Thread(target=run, args=(i,))：初始化线程。
     #   -target：绑定启动线程时需要执行的方法。
     #   -args：以元组形式传递run()需要的的参数。
-    #   -daemon：主动指定是否为守护线程True/False。意味着程序必须要等待守护线程结束后才能退出。
-    #            默认为None，意味着new_thread=current_thread().daemon。
-    #            通常当前线程是主线程，主线程不是守护线程，main_thread.daemon=False，由主线程创建的线程都不是守护线程。（搞反了）
-    # 一个线程可以被标记成一个“守护线程”。 这个标识的意义是，当剩下的线程都是守护线程时，整个 Python 程序将会退出。 初始值继承于创建线程。
-    # 这个标识可以通过 daemon 特征属性或者 daemon 构造器参数来设置。
-    threading_list.append(Thread(target=run_long, args=("task run_long",)))
-    # threading_list.append(Thread(target=run_short, args=("task run_short",)))
-    threading_list.append(Thread(target=run_short, args=("task run_short",), daemon=True))
-    # todo：do some work
-
+    #   -daemon：主动指定是否为守护线程True/False。默认为None，意味着new_thread.daemon=current_thread().daemon。
+    threading_list.append(Thread(target=run_task1))
+    threading_list.append(Thread(target=run_task2, daemon=True))
     for th in threading_list:
         # 启动线程
         th.start()
 
-    # data = [i for i in range(10)]
-    # for d in data:
-    #     Thread(target=run, args=(d,)).start()
-
-    # 下面介绍几种线程常用方法
-    # join([timeout])：阻塞当前线程，直至thread终结。
-    #   -可能是正常终结/抛出异常，线程不存活。
-    #   -可能是超时终结，线程会仍然存活。
-    # threading_list[0].join()
-    # do sth join（）作为一种条件，而不是等待结束。
-    # 此方法结束了，但主线程仍然会等待子线程执行完毕后再结束。线程一旦启动后，就开始独立地运行，知道run方法结束（除了daemon线程）
-    # 解释器会一直保持运行，直到所有的非daemon线程都终止。（验证一下）
-
-    # is_alive()：线程时是否还在运行
+    # join([timeout])：默认timeout为None，意味着永久等待。
+    #   -阻塞当前线程，直至thread终结或者超时，共3种情况：1)thread run正常执行结束。2)thread run异常结束。3)thread未结束，等待超时。
+    threading_list[-1].join(60)
+    # is_alive()：判断线程是否在运行
     #   -返回True：当run()方法刚开始直到run()方法刚结束。
-    #   -返回False：run()方法结束后，无论以任何方式结束，正常结束or异常抛出
-    # threading_list[0].is_alive()
+    #   -返回False：run()方法结束后，无论run()正常/异常结束都算。
+    # join()之后在判断线程状态，避免join()后线程仍然存活的情况。
+    if threading_list[-1].is_alive():
+        raise RuntimeError("thread could not stop running")
+    print("在threading_list[-1]执行完毕的条件下，做一些运算。")
+
+    # 下方介绍例子2：多线程执行多个 无序性的、独立的、相同的子任务
+    datas = [i for i in range(10)]
+    for data in datas:
+        Thread(target=run, args=(data,)).start()
+
+    print("方法结束>>> fun_name={} thread_name={}".format(new_threading.__name__, threading.current_thread().name))
 
 
 """实现多线程-方式2：继承threading.Thread类"""
@@ -119,7 +112,8 @@ def lock_by_finally():
         # acquire([,blocking=True][,timeout=-1])：上锁。
         #   -blocking: 默认为True。意味着如果已经是锁定状态，将阻塞当前线程直到锁被释放，然后立刻上锁。
         #   -timeout: 仅适用于blocking=True。指定最长阻塞时间，超时返回False；默认为-1时，表示无限等待。
-        lock.acquire()
+        # 多个线程被锁阻塞，并不是谁先等谁先拿到锁，顺序无法确定。
+        # lock.acquire()
 
         # 不支持重复调用锁，此处再次调用会引发没有release而永久等待锁的问题。
         # lock.acquire()
@@ -130,8 +124,9 @@ def lock_by_finally():
         # lock.release()
 
     # 锁同步状态的例子 need better one only 状态 not变量
-    while not lock.locked():
+    while lock.locked():
         print("waiting for thread end")
+        time.sleep(3)
     print("li: {}".format(li))
 
 
@@ -140,6 +135,7 @@ def run_lock_finally(li, lock):
     # lock.acquire()
 
     try:
+        lock.acquire()
         li.append(threading.current_thread().name)
         print("thread:{}, li:{}".format(threading.current_thread().name,
                                         li))  # 如果不上锁，print语句执行前就会让出GIL，导致print前后li[-1]并不一致。
@@ -153,16 +149,22 @@ def run_lock_finally(li, lock):
 
 def lock_by_with():
     """2）使用with-as语句释放锁"""
+    # lock避免每次都创建一个新的
     li = []
+    lock = Lock()
     for i in range(5):
-        Thread(target=run_lock_with, args=(li,)).start()
+        Thread(target=run_lock_with, args=(li, lock)).start()
     print("li: {}".format(li))
 
 
-def run_lock_with(li):
-    # 进入语句块时，acquire()会被调用。
+# lock = Lock()
+
+
+def run_lock_with(li, lock):
+    # 进入语句块时，具体是执行缩进行时调用acquire()会被调用。
     # 退出语句块时，release()会被调用。
-    with Lock():
+
+    with lock:
         li.append(threading.current_thread().name)
         print("thread:{}, li:{}".format(threading.current_thread().name, li))
         li[-1] += "mo"
@@ -218,7 +220,7 @@ def run_rlock_by_finally(li, rlock):
     print()
 
 
-"""死锁-场景1：因程序设计，导致某些情况下没有执行释放锁的语句，导致其他线程无法再使用资源。"""
+"""死锁-场景1：因程序设计，导致某些情况下没有执行释放锁的语句，导致其他线程无法再使用资源。try——finally/with"""
 
 deadlock_unreleased = Lock()
 deadlock_unreleased_list = [1, 2, 3]
@@ -233,17 +235,19 @@ def new_deadlock_unreleased(index):
     deadlock_unreleased.release()
     return True
 
+"""死锁：常见原因尝试获取了多个锁"""
+
 
 if __name__ == "__main__":
     # 主线程
     print(threading.current_thread().name, threading.current_thread().daemon)
 
     # 实现多线程
-    new_threading()
+    # new_threading()
 
     # 线程锁
     # lock_by_finally()
-    # lock_by_with()
+    lock_by_with()
     # rlock_threading()
 
     # 死锁举例
